63dc94dfecfef905a068f95a7c9ff1e4d9dbf5ef
Juan Kasem <juan.kasem@virtudev.net>
2021-02-07 16:41:35 +0300

    install crypto-js package

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,4 +1,5 @@
-const crypto = require('crypto');
+// const crypto = require('crypto');
+const SHA256 = require('crypto-js/sha256');
 const EC = require('elliptic').ec
 const ec = new EC('secp256k1');
 
@@ -16,7 +17,7 @@ class Transaction{
    * @returns {string}
    */
     calculateHash(){
-         return crypto.createHash('sha256').update(this.fromAddress, this.toAddress, this.amount + this.timestamp).digest('hex');
+         return SHA256(this.fromAddress, this.toAddress, this.amount + this.timestamp).toString();
     }
 
      /**
@@ -73,7 +74,7 @@ class Block{
    * @returns {string}
    */
     calculateHash(){
-      return crypto.createHash('sha256').update(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).digest('hex');
+      return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
     }
    
      /**
